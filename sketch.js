e1f4c810be43aca8120e39f7f78a3e1b1441833c
healthyego <39567694+healthyego@users.noreply.github.com>
2018-07-18 15:15:07 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+
+
+
+function setup(){
+ background(0);
+ createCanvas(1080,1920);
+ vid= createVideo('Websitefootage.mov');
+ vid.loop();
+
+
+ instagram= createImg('Instagram.png','clickable');
+ facebook= createImg('Facebook.png','clickable');
+ HEGarten= createImg('HEGarten.png','clickable');
+
+
+
+
+}
+
+function draw(){
+
+
+
+  push();
+
+  HEGarten.position(0,0);
+  HEGarten.size(1080, 595)
+  //instagram.mousePressed(instagramLink);
+  pop();
+
+
+  instagram.position(1080/2-64,windowHeight-100);
+  instagram.size(64,64);
+  instagram.mousePressed(instagramLink);
+
+  facebook.position(1080/2+64,windowHeight-100);
+  facebook.size(64,64);
+  facebook.mousePressed(facebookLink);
+
+  push();
+  image(vid,0,0);
+  vid.hide();
+  pop();
+
+  textSize(50);
+  fill(0);
+  text('Freitag 19-23 Uhr',400,700);
+  text('Samstag 19-23 Uhr',400,800);
+  text('Sonntag 19-23 Uhr',400,900);
+}
+
+
+
+
+
+function facebookLink(){
+ window.open("https://m.facebook.com/Healthy-Ego-993923804114169/");
+ }
+
+function instagramLink(){
+ window.open("https://instagram.com/haelthyego");
+ }
