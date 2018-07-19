14828fc8f3161ccdc42237a6402d729b73533836
healthyego <39567694+healthyego@users.noreply.github.com>
2018-07-19 10:25:13 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,31 +7,20 @@ function preload(){
 
 
 function setup(){
- background(img);
  createCanvas(1080,1920);
- 
-
+ background(img);
 
  instagram= createImg('Instagram.png','clickable');
  facebook= createImg('Facebook.png','clickable');
  HEGarten= createImg('HEGarten.png','clickable');
-
-
-
-
 }
 
 function draw(){
-
-
-
   push();
-
   HEGarten.position(0,0);
   HEGarten.size(1080, 595)
   //instagram.mousePressed(instagramLink);
   pop();
-
 
   instagram.position(1080/2-64,1600);
   instagram.size(64,64);
@@ -41,12 +30,14 @@ function draw(){
   facebook.size(64,64);
   facebook.mousePressed(facebookLink);
 
+
   textSize(50);
  if (n=4){
   fill(100,100,20);
  }else{
   fill(0);
  }
+ 
   text('Freitag 19-23 Uhr',350,700);
   text('Samstag 17-22 Uhr',350,800);
   text('Sonntag 15-20 Uhr',350,900);
