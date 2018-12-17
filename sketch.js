7229c5fa3836783eea72bc4ebd3f1668fb7c3dbf
healthyego <39567694+healthyego@users.noreply.github.com>
2018-12-17 08:15:09 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-
+var n;
 var img;
 
 function preload(){
@@ -7,48 +7,18 @@ function preload(){
 
 
 function setup(){
+ n=random(0,1);
  createCanvas(1080,1920);
  background(img);
-
- instagram= createImg('Instagram.png','clickable');
- facebook= createImg('Facebook.png','clickable');
- //HEGarten= createImg('HEGarten.png','clickable');
 }
 
 function draw(){
-  push();
-  HEGarten.position(0,0);
-  HEGarten.size(1080, 595)
-  //instagram.mousePressed(instagramLink);
-  pop();
-
-  instagram.position(1080/2-64,1600);
-  instagram.size(64,64);
-  instagram.mousePressed(instagramLink);
-
-  facebook.position(1080/2+64,1600);
-  facebook.size(64,64);
-  facebook.mousePressed(facebookLink);
-
-
-  textSize(50);
-
-  fill(0);
  
- 
-  text('Freitag 19-23 Uhr',320,700);
-  text('Samstag 17-22 Uhr',320,800);
-  text('Sonntag 15-20 Uhr',320,900);
+ function mousePressed(){
+  if(n<=0.7){
+   window.open(https://instagram.com/haelthyego)
+  }else{
+   window.open(https://www.instagram.com/steamwaves.studios/)
+  }
+ }
 }
-
-
-
-
-
-function facebookLink(){
- window.open("https://m.facebook.com/Healthy-Ego-993923804114169/");
- }
-
-function instagramLink(){
- window.open("https://instagram.com/haelthyego");
- }
