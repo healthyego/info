0b1182fcf3befd9ec065423b8a80edda5e4f95c2
healthyego <39567694+healthyego@users.noreply.github.com>
2018-12-18 11:16:10 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,14 @@
-var n;
-var img;
-
-
-
-
-function setup(){
- img=createImage("Entwurf2b.png");
- n=random(0,1);
- createCanvas(1080,1920);
- background(img);
-}
- 
- function mousePressed(){
+function mousePressed(){
  // window.open("https://instagram.com/haelthyego/")
 }
 
+function setup(){
+ img = loadImage('Entwurf2b.png');
+ createCanvas(windowWidth, windowHeight);
+ background(80);
+}
+
+function draw(){ 
+ imageMode(CENTER);
+ image(img,windowWidth/2,windowHeight/2,width,width/1.777); 
+}
